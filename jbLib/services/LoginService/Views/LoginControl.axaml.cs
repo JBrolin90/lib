a7d506679ce48358c7a7cf8e8e66ed234907b0d5
Joachim Brolin <jbrolin90@gmail.com>
2024-11-22 14:55:46 +0100

    Added SQL server Login Service

---
+++ jbLib/services/LoginService/Views/LoginControl.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace jbLib.services.LoginService.Views;
+
+public partial class LoginControl : UserControl
+{
+    public LoginControl()
+    {
+        InitializeComponent();
+    }
+}
